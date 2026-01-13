da20284574fa2c4fbedcac12baa4fc41b06d1f54
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-01-13 04:27:51 -0800

    added forward and back movement.  bound movement keys to use wasd or arrows

--- player.py
+++ player.py
@@ -17,17 +17,32 @@ class Player(CircleShape):
     
     def draw(self, screen):
         player_triangle = self.triangle()
-        print(f"debug: player trinagle: {player_triangle}")
         pygame.draw.polygon(screen, "white", player_triangle, LINE_WIDTH)
     
     def rotate(self, dt):
         self.rotation = self.rotation + (PLAYER_TURN_SPEED * dt)
     
+    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector   
+    
+    
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.rotate(-dt)
 
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.rotate(dt)
+        
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.move(-dt)
+    
+    
