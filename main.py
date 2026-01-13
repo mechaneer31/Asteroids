99a1ae7207b495685394c35cddd3ba84af423c75
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-01-13 02:27:39 -0800

    Set up pygame screen to appear, black background, close button on screen works

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from logger import log_state
 
 
 def main():
@@ -7,6 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
+
